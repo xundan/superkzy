b932eabe0de3a4c2e7558070a571ee524cde47b3
xuncl <xuncl@126.com>
2016-11-28 10:05:11 +0800

    show personal info from session
    rv by lx

--- Application/Home/Controller/PersonalCenterController.class.php
+++ Application/Home/Controller/PersonalCenterController.class.php
@@ -13,6 +13,10 @@ header("Content-type: text/html; charset=utf-8");
 class PersonalCenterController extends ComController
 {
     public function personal_center(){
+        $user_info = $_SESSION['user_info'];
+        $this->assign('user_info',$user_info);
+        $user_role = session('user_info')['role_id']==null?"游客":session('user_info')['role_id'];
+        $this->assign('user_role',$user_role);
         $this->display();
     }
 
