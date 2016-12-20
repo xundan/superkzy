aa7b3da507de71107a76cc522afcd15e2f499446
xuncl <xuncl@126.com>
2016-12-20 15:49:22 +0800

    create 4 kinds of card
    rv by lx

---
+++ Application/Home/Common/CardList/CarGiveMsgCard.class.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CLEVO
+ * Date: 2016/12/20
+ * Time: 11:36
+ */
+
+namespace Home\Common\CardList;
+
+
+class CarGiveMsgCard extends MsgCard
+{
+
+    function toLi()
+    {
+        $message = $this->_message;
+        $personal_page = $this->getPersonalUrl();
+        if ($message['is_formatted']) { // 如果用户按照标准格式填写
+            $li_str = "<li class=\"weui_panel weui_panel_access\" style=\"border-radius: 5px\">
+<div class=\"weui_media_box weui_media_appmsg\" style=\"margin: 0;padding-left: 0;padding-right: 0\">
+    <a href=\"{$personal_page}\">
+        <div class=\"weui_media_hd\">
+            <img src=\"{$message['heading_url']}\" class=\"weui_media_appmsg_thumb\">
+        </div>
+    </a>
+    <div class=\"weui_media_bd\">
+        <a href=\"{$personal_page}\">
+            <h4 class=\"weui_media_title\">{$message['user_name']}<img src=\"__PUBLIC__/home/images/medal.png\" style=\"width: 15px;height: 20px\"></h4>
+        </a>
+        <p class=\"weui_media_desc\">
+            <img src=\"__PUBLIC__/home/images/area_start.png\" style=\"width: 15px;height: 20px\">
+            {$message['area_start']}→{$message['area_end']}
+            <img src=\"__PUBLIC__/home/images/area_end.png\" style=\"width: 15px;height: 20px\">
+        </p>
+    </div>
+    <div class=\"weui_media_bd\" style=\"margin-right: -20%\">
+        <a href=\"tel:{$message['phone_number']}\" class=\"\">
+            <h4><img src=\"__PUBLIC__/home/images/phone.png\" class=\"\" style=\"width: 20px\">
+                <span style=\"\">拨打电话</span></h4>
+        </a>
+        <div>
+            <button class=\"btn btn-xs btn-default\" style=\"padding-left: 20%;padding-right: 20%;border-color: #04bfc6;color: #04bfc6\">{$message['in_collection']}</button>
+        </div>
+    </div>
+</div>
+<table class=\"table table-condensed\" style=\"margin: 0\">
+    <tbody>
+    <tr><td>车辆类型:{$message['car_type']}</td><td>吨数:{$message['car_capacity']}</td></tr>
+    <tr><td></td><td>发布时间:{$message['publish_time']}</td></tr>
+    </tbody>
+</table>
+</li>";
+        } else {
+            $li_str = "<li class=\"weui_panel weui_panel_access\" style=\"border-radius: 5px\">
+<div class=\"weui_media_box weui_media_appmsg\" style=\"margin: 0;padding-left: 0;padding-right: 0\">
+    <a href=\"{$personal_page}\">
+        <div class=\"weui_media_hd\">
+            <img src=\"{$message['heading_url']}\" class=\"weui_media_appmsg_thumb\">
+        </div>
+    </a>
+    <div class=\"weui_media_bd\">
+        <a href=\"{$personal_page}\">
+            <h4 class=\"weui_media_title\">{$message['user_name']}<img src=\"__PUBLIC__/home/images/medal.png\" style=\"width: 15px;height: 20px\"></h4>
+        </a>
+        <p class=\"weui_media_desc\">
+            <img src=\"__PUBLIC__/home/images/area_start.png\" style=\"width: 15px;height: 20px\">
+            {$message['area_start']}→{$message['area_end']}
+            <img src=\"__PUBLIC__/home/images/area_end.png\" style=\"width: 15px;height: 20px\">
+        </p>
+    </div>
+    <div class=\"weui_media_bd\" style=\"margin-right: -20%\">
+        <a href=\"tel:{$message['phone_number']}\" class=\"\">
+            <h4><img src=\"__PUBLIC__/home/images/phone.png\" class=\"\" style=\"width: 20px\">
+                <span style=\"\">拨打电话</span></h4>
+        </a>
+        <div>
+            <button class=\"btn btn-xs btn-default\" style=\"padding-left: 20%;padding-right: 20%;border-color: #04bfc6;color: #04bfc6\">{$message['in_collection']}</button>
+        </div>
+    </div>
+</div>
+<div>{$message['content']}</div><div class='pull-right'>发布时间:{$message['publish_time']}</div>
+</li>";
+        }
+        return $this->replacePublicString($li_str);
+    }
+
+
+    protected function getOrderDetailUrl()
+    {
+        return U('DriverOrder/driver_order_detail',array("id"=>$this->_message['id']));
+    }
+
+}
--- Application/Home/Common/CardList/MsgCard.class.php
+++ Application/Home/Common/CardList/MsgCard.class.php
@@ -23,7 +23,8 @@ class MsgCard extends Card
      * @param $messages
      * @return array
      */
-    function createMsgCardArray($messages){
+    function createMsgCardArray($messages)
+    {
         $resultArray = array();
         foreach ($messages as $message) {
             $msgCard = new MsgCard($message);
@@ -37,11 +38,13 @@ class MsgCard extends Card
         return $this->_message['title'];
     }
 
-    function hashCode(){
+    function hashCode()
+    {
 
     }
 
-    function equals(Card $card){
+    function equals(Card $card)
+    {
 
     }
 
@@ -49,96 +52,47 @@ class MsgCard extends Card
     {
         $li_str = "";
         $message = $this->_message;
-        if ($message['type']=='plain'){
-            $li_str = "<li>".$message['content']."</li>";
-        }else{
-            if($message['category']==0){ // 供应
-                $li_str = "<li>".$this->_message['content']."</li>";
+        if ($message['type'] == 'plain') { // R
+            $li_str = "<li>" . $message['content'] . "</li>";
+        } else {
+            if ($message['category'] == 0) { // 供应
+                $li_str = "<li>" . $this->_message['content'] . "</li>";
                 $li_str = "";
-            }elseif($message['category']==1){ // 司机找活
-                $li_str = "<li>".$this->_message['content']."</li>";
-            }elseif($this->_message['category']==2){ // 求购
-                $li_str = "<li class=\"weui_panel weui_panel_access\" style=\";border-radius: 5px\">
-    <div style=\"position: absolute;right: 0px;\">
-        < img src=\"__PUBLIC__/home/images/buy.png\" width=\"50px\">
-    </div>
-    <div class=\"weui_media_box weui_media_appmsg\" style=\"margin: 0;padding-left: 0;padding-right: 0\">
-        < a href=\" \">
-            <div class=\"weui_media_hd\">
-                < img src=\"__PUBLIC__/home/images/headimg_background.png\" class=\"weui_media_appmsg_thumb\">
-            </div>
-        </ a>
-        <div class=\"weui_media_bd\">
-            < a href=\"#\">
-                <h4 class=\"weui_media_title\">{名字} < img src=\"__PUBLIC__/home/images/medal.png\" style=\"width: 15px;height: 20px\"></h4>
-            </ a>
-            <p class=\"weui_media_desc\">
-                < img src=\"__PUBLIC__/home/images/area_start.png\" style=\"width: 10px;height: 15px\">
-                {}
-            </p >
-        </div>
-        <div class=\"weui_media_bd text-center\" style=\"\">
-            < a href=\"tel:15153151316\" class=\"\">
-                <h4>
-                    < img src=\"__PUBLIC__/home/images/phone.png\" class=\"\" style=\"width: 20px\">
-                    <span style=\"\">拨打电话</span>
-                </h4>
-            </ a>
-            <div>
-                <button class=\"btn btn-xs btn-default\" style=\"padding-left: 20%;padding-right: 20%;border-color: #04bfc6;color: #04bfc6\">
-                    收藏
-                </button>
-            </div>
-        </div>
-    </div>
-    <!--<div class=\"panel-body\" style=\"padding:0\">-->
-    <table class=\"table table-condensed\" style=\"margin: 0\">
-        <tbody>
-        <tr>
-            <td>
-                煤炭种类:{}
-            </td>
-            <td>
-                {$order['price']}元/吨
-            </td>
-        </tr>
-        <tr>
-            <td>
-                煤炭品质:{}
-            </td>
-            <td>
-                产地:{$order['area_start']}
-            </td>
-        </tr>
-        <tr>
-            <td>
-                煤炭粒度:{}
-            </td>
-            <td>
-                吨数:{}
-            </td>
-        </tr>";
-            }elseif($this->_message['category']==3){ // 找车
-                $li_str = "<li>".$this->_message['content']."</li>";
-            }else{
+            } elseif ($message['category'] == 1) { // 司机找活
+                $li_str = "<li>" . $this->_message['content'] . "</li>";
+            } elseif ($this->_message['category'] == 2) { // 求购
+                $li_str = "";
+            } elseif ($this->_message['category'] == 3) { // 找车
+                $li_str = "<li>" . $this->_message['content'] . "</li>";
+            } else {
                 // todo log here.
             }
         }
-        return $li_str;
+        return $this->replacePublicString($li_str);
 
     }
 
-//    function toSimple(){ // sometimes naive
-//        if ($this->_message['type']=='plain'){ //微信的消息
-//            $new_message['title']=$this->_message['title'];
-//            $new_message['phone_number']=$this->_message['origin'];
-//            $new_message['content']=$this->_message['content'];
-//            $this->_message=$new_message;
-//        }else{
-//            if($this->_message['category']==0){
-//
-//            }
-//        }
-//    }
+    protected function replacePublicString($str)
+    {
+        $real_public_str = __ROOT__ . '/Public';
+        return str_replace("__PUBLIC__", $real_public_str, $str);
+    }
 
+    protected function getPersonalUrl()
+    {
+        $role_id = $this->_message['role_id'];
+        $personal_page = "";
+        if ($role_id == 1) {
+            $personal_page = U('PersonalCenter/driver_data', array('uid' => $this->_message['uid']));
+        } elseif ($role_id == 2) {
+            $personal_page = U('PersonalCenter/owner_data', array('uid' => $this->_message['uid']));
+        } else {
+        }
+        return $personal_page;
+    }
+
+    protected function getOrderDetailUrl(){
+        // 返回一个静态文件
+        return __ROOT__.'/Public/home/OrderFromWX.html';
+    }
 }
