70a838449b68faec8dc0472275f295aa738bdf84
xuncl <xuncl@126.com>
2017-01-05 09:31:14 +0800

    make search controller hierarchy
    rv by lx

--- Application/Home/Controller/DriverSearchController.class.php
+++ Application/Home/Controller/DriverSearchController.class.php
@@ -8,19 +8,15 @@
 
 namespace Home\Controller;
 
-use Home\Common\CardList\CardList;
 use Home\Common\CardList\WhereConditions;
-use Home\Model\MessagesModel;
 use Think\Controller;
 
 
-class DriverSearchController extends ComController
+class DriverSearchController extends SearchController
 {
     public function driver_job_search()
     {
-        $input = $this->acquireInput();
-
-        $whereCond = $this->createNewWhereConditions($input);
+        $whereCond = $this->createNewWhereConditions();
 
         $data = $this->getOrderWithoutExist($whereCond,0);
 
@@ -44,87 +40,11 @@ class DriverSearchController extends ComController
         return;
     }
 
-    /**
-     * 在后台拼装WhereConditions对象
-     */
-    public function setCookie()
-    {
-        $post = I('post.', '', 'trim,strip_tags');
-        $area_start_id = $post['area_start_id'];
-        $area_end_id = $post['area_end_id'];
-        $whereCond = new WhereConditions();
-        $whereCond->pushCond("area_start", "eq", $area_start_id);
-        $whereCond->pushCond("area_end", "eq", $area_end_id);
-        $whereCond->pushCond("area_start", "like", $area_end_id);
-        $whereCond->pushCond("area_end", "like", "4101%");
-        $whereCond->pushCond("area_start", "like", "61%");
-        $whereCond->pushCond("area_end", "like", "41%");
-
-        cookie("where_cond_json", $whereCond->toJson());
-    }
-
 
     /**
-     * 获取返回数据，保证每次都获取十个卡片，包括tips
-     * @param $whereCond mixed 查询条件
-     * @param $stage int 当前卡片列表的状态：精确、模糊、其他、结束
-     * @return mixed 返回数据
+     * @return WhereConditions
      */
-    public function getOrderWithoutExist(WhereConditions $whereCond, $stage)
-    {
-        if ($stage < CardList::END) { // 如果是结束阶段就不执行下面代码了
-            $Msg = new MessagesModel();
-            $cards = new CardList(array());
-            $cards->setStage($stage);
-            $count = "0";
-            do {
-                $isPopped = $whereCond->preSQL();
-                if($cards->notFull()&&$isPopped&&$cards->atAccurate()){// 如果是第一次退格约束
-                    $count.="1";
-                    $cards->addSimilar();
-                    if ($cards->isFull()){
-                        $count.="b";
-                        break;
-                    }
-                }
-                if($cards->notFull()&&$whereCond->isExhausted()&&$cards->atSimilar()){// 如果条件退完
-                    $count.="2";
-                    $cards->addOther();
-                    if ($cards->isFull()){
-                        $count.="c";
-                        break;
-                    }
-                }
-                $temp_messages = $Msg->findWhereWithoutExist($whereCond, $cards->getCount(),"找车");
-                $cards->appendMessage($temp_messages);
-                if ($cards->atOther()&&$cards->notFull()){ //其他查询也不能满足，说明查到底了
-                    $count.="3";
-                    $count.="d[".$cards->getCount()."]";
-                    $cards->addEnd();
-                    break;
-                }
-                $whereCond->postSQL($temp_messages, $cards->getCount());
-                $count.="a[".$cards->getCount()."]";
-            } while (!($cards->isFull() || $cards->atEnd()));
-            // 把$whereCond送到前台
-            $data["msg"] = "".$count;
-            $data['where_cond_json'] = $whereCond->toJson();
-            $data["li_array"] = $cards->toLiArray();
-            $data["stage"] = $cards->getStage();
-        } else {
-            $data["msg"] = "success";
-            $data['where_cond_json'] = $whereCond->toJson();
-            $data["li_array"] = "";
-            $data["stage"] = $stage;
-        }
-
-        return $data;
-    }
-
-    /**
-     * @return mixed
-     */
-    private function acquireInput()
+    function createNewWhereConditions()
     {
         $post = I('post.', '', 'trim,strip_tags');
         if ($post) {
@@ -132,28 +52,18 @@ class DriverSearchController extends ComController
             $input['areaEnd'] = $post['area_end'];
             $input['searchInput'] = $post['search_input'];
             $input['searchTag'] = $post['select_category'];
-            return $input;
         } elseif (cookie('search_tag')) {
             $input['areaStart'] = cookie('area_start_id');
             $input['areaEnd'] = cookie('area_end_id');
             $input['searchInput'] = cookie('search_input');
             $input['searchTag'] = cookie('search_tag');
-            return $input;
         } else {
             $input['areaStart'] = "610800";
             $input['areaEnd'] = "410100";
             $input['searchInput'] = "";
             $input['searchTag'] = "all";
-            return $input;
         }
-    }
 
-    /**
-     * @param $input
-     * @return WhereConditions
-     */
-    private function createNewWhereConditions($input)
-    {
         $whereCond = new WhereConditions();
         if ($input['searchTag'] == "all") {
             $whereCond->pushSearchCond("content_all", $input['searchInput']);
---
+++ Application/Home/Controller/SearchController.class.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CLEVO
+ * Date: 2017/1/5
+ * Time: 9:20
+ */
+
+namespace Home\Controller;
+
+use Home\Common\CardList\CardList;
+use Home\Common\CardList\WhereConditions;
+use Home\Model\MessagesModel;
+use Think\Controller;
+
+abstract class SearchController extends ComController
+{
+
+
+    /**
+     * 获取返回数据，保证每次都获取十个卡片，包括tips
+     * @param $whereCond mixed 查询条件
+     * @param $stage int 当前卡片列表的状态：精确、模糊、其他、结束
+     * @return mixed 返回数据
+     */
+    public function getOrderWithoutExist(WhereConditions $whereCond, $stage)
+    {
+        if ($stage < CardList::END) { // 如果是结束阶段就不执行下面代码了
+            $Msg = new MessagesModel();
+            $cards = new CardList(array());
+            $cards->setStage($stage);
+            $count = "0"; // 调试用，查看do/while的行为，数字表示阶段，字母表示结束一次迭代的方式。
+            do {
+                $isPopped = $whereCond->preSQL();
+                if($cards->notFull()&&$isPopped&&$cards->atAccurate()){// 如果是第一次退格约束
+                    $count.="1";
+                    $cards->addSimilar();
+                    if ($cards->isFull()){
+                        $count.="b";
+                        break;
+                    }
+                }
+                if($cards->notFull()&&$whereCond->isExhausted()&&$cards->atSimilar()){// 如果条件退完
+                    $count.="2";
+                    $cards->addOther();
+                    if ($cards->isFull()){
+                        $count.="c";
+                        break;
+                    }
+                }
+                $temp_messages = $Msg->findWhereWithoutExist($whereCond, $cards->getCount(),"找车");
+                $cards->appendMessage($temp_messages);
+                if ($cards->atOther()&&$cards->notFull()){ //其他查询也不能满足，说明查到底了
+                    $count.="3";
+                    $count.="d[".$cards->getCount()."]";
+                    $cards->addEnd();
+                    break;
+                }
+                $whereCond->postSQL($temp_messages, $cards->getCount());
+                $count.="a[".$cards->getCount()."]";
+            } while (!($cards->isFull() || $cards->atEnd()));
+            // 把$whereCond送到前台
+            $data["msg"] = "".$count;
+            $data['where_cond_json'] = $whereCond->toJson();
+            $data["li_array"] = $cards->toLiArray();
+            $data["stage"] = $cards->getStage();
+        } else {
+            $data["msg"] = "success";
+            $data['where_cond_json'] = $whereCond->toJson();
+            $data["li_array"] = "";
+            $data["stage"] = $stage;
+        }
+
+        return $data;
+    }
+
+
+    /**
+     * 根据输入条件组装WhereCondition
+     * @return WhereConditions
+     */
+    abstract function createNewWhereConditions();
+
+}
