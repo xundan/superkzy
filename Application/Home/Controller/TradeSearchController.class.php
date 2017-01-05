d971504501cc3c3ddde9baedacb672a3c09ba9e2
xuncl <xuncl@126.com>
2017-01-05 15:54:16 +0800

    update trade search
    rv by lx

--- Application/Home/Controller/TradeSearchController.class.php
+++ Application/Home/Controller/TradeSearchController.class.php
@@ -18,7 +18,7 @@ class TradeSearchController extends SearchController
 
         $whereCond = $this->createNewWhereConditions($input);
 
-        $data = $this->getOrderWithoutExist($whereCond,0,"其他");
+        $data = $this->getOrderWithoutExist($whereCond,0,$input['searchTag']);
 
         $this->assign("li_array", $data['li_array']);
         $this->assign("where_cond_json", $data['where_cond_json']);
@@ -49,17 +49,13 @@ class TradeSearchController extends SearchController
     function createNewWhereConditions($input)
     {
         $whereCond = new WhereConditions();
-        if ($input['searchTag'] == "all") {
-            $whereCond->pushSearchCond("content_all", $input['searchInput']);
-            return $whereCond;
-        } else {
-            $whereCond->pushSearchCond("content_all", $input['searchInput']);
-            $whereCond->pushCond("area_start", "like", substr($input['areaStart'], 0, 2) . "%");
-            $whereCond->pushCond("area_end", "like", substr($input['areaEnd'], 0, 2) . "%");
-            $whereCond->pushCond("area_start", "eq", $input['areaStart']);
-            $whereCond->pushCond("area_end", "eq", $input['areaEnd']);
-            return $whereCond;
-        }
+        $whereCond->pushSearchCond("content_all", $input['searchInput']);
+        $whereCond->pushCond("kind","in",$input['filter_kind']);
+        $whereCond->pushCond("granularity","in",$input['filter_granularity']);
+        $whereCond->pushCond("heat_value_min","not lt",$input['filter_heat_min']);
+        $whereCond->pushCond("heat_value_max","not gt",$input['filter_heat_max']);
+
+        return $whereCond;
     }
 
     /**
@@ -69,22 +65,20 @@ class TradeSearchController extends SearchController
     {
         $post = I('post.', '', 'trim,strip_tags');
         if ($post) {
-            $input['areaStart'] = $post['area_start'];
-            $input['areaEnd'] = $post['area_end'];
             $input['searchInput'] = $post['search_input'];
             $input['searchTag'] = $post['select_category'];
-            return $input;
-        } elseif (cookie('search_tag')) {
-            $input['areaStart'] = cookie('area_start_id');
-            $input['areaEnd'] = cookie('area_end_id');
-            $input['searchInput'] = cookie('search_input');
-            $input['searchTag'] = cookie('search_tag');
+            $input['filter_kind'] = $post['filter_kind'];
+            $input['filter_granularity'] = $post['filter_granularity'];
+            $input['filter_heat_min'] = $post['filter_heat_min'];
+            $input['filter_heat_max'] = $post['filter_heat_max'];
             return $input;
         } else {
-            $input['areaStart'] = "610800";
-            $input['areaEnd'] = "410100";
             $input['searchInput'] = "";
-            $input['searchTag'] = "all";
+            $input['searchTag'] = "求购";
+            $input['filter_kind'] = null;
+            $input['filter_granularity'] = null;
+            $input['filter_heat_min'] = null;
+            $input['filter_heat_max'] = null;
             return $input;
         }
     }
